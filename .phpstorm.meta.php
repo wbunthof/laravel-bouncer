df26e8c5bbd669d6581a26320a9b314c9f9379e2
woetroe0803 <wbunthof@gmail.com>
2019-04-24 18:36:33 +0200

    Start with Bouncer, Made a seed with 3 default roles and 3 users assigned to them. Also after login displayed name and role(s).

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-04-24 12:15:14.
+    * Generated on 2019-04-24 13:59:51.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
